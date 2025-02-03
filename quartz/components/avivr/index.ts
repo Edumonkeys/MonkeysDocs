232ee9ddd669c6d54ea370b22f1d4ebfa7285020
Edumonkeys <edumonkeyverse@gmail.com>
2025-02-03 23:58:15 +0530

    Delete quartz/components/avivr directory

--- quartz/components/avivr/index.ts
+++
@@ -1,6 +0,0 @@
-import Footer from "./Footer"
-import Navbar from "./Navbar"
-import MultiComponentContainer from "./MultiComponentContainer"
-import ConditionalComponent from "./ConditionalComponent"
-
-export { Footer, Navbar, MultiComponentContainer, ConditionalComponent }
